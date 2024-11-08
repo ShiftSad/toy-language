a8b332cbec45c3b75ab153112663fa16fc1fca06
Sadness <rosalimavictor@gmail.com>
2024-11-07 22:52:40 -0300

    Allow for printing big numbers even when they are non-decimal

--- src/main/java/org/example/toylanguage/expression/value/NumericValue.java
+++ src/main/java/org/example/toylanguage/expression/value/NumericValue.java
@@ -8,7 +8,7 @@ public class NumericValue extends ComparableValue<Double> {
     @Override
     public String toString() {
         if ((getValue() % 1) == 0)
-            return String.valueOf(getValue().intValue());
+            return String.valueOf(getValue()).split("\\.")[0];
         return super.toString();
     }
 }
